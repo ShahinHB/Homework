b4c195e64737e25d6ffe17be1e47795343106957
shahin.babayev <shahin.babayev@emanat.az>
2024-03-25 16:59:44 +0400

    React native intro

--- react-native-practice/App.js
+++ react-native-practice/App.js
@@ -1,22 +1,51 @@
-import React from 'react';
-import { StatusBar, Button, StyleSheet, Text, View, Alert } from 'react-native';
+import React, { useState } from 'react';
+import { StatusBar, Button, StyleSheet, Text, View, TextInput, Alert } from 'react-native';
 
 export default function App() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [message, setMessage] = useState('');
 
-  const showAlert = () => {
-    Alert.alert(
-      "Test",
-      "Test!",
-      [
-        { text: "OK", onPress: () => console.log("Pressed") }
-      ]
-    );
+  const handleLogin = async () => {
+    try {
+      const response = await fetch('https://recruitment-api.pyt1.stg.jmr.pl/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          login: username,
+          password: password
+        })
+      });
+      const data = await response.json();
+      if (response.ok) {
+        setMessage(data.message);
+      } else {
+        throw new Error(data.message);
+      }
+    } catch (error) {
+      Alert.alert('Error', error.message);
+    }
   };
 
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Button title="Test button" onPress={showAlert} />
+      <TextInput
+        style={styles.input}
+        placeholder="Username"
+        onChangeText={text => setUsername(text)}
+        value={username}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        onChangeText={text => setPassword(text)}
+        value={password}
+        secureTextEntry={true}
+      />
+      <Button title="Login" onPress={handleLogin} />
+      <Text>{message}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -27,5 +56,12 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  input: {
+    width: '80%',
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
   },
 });
